981852b196b2bcfdcdd644fce2402a6ec23cad47
Sean <e0406739@u.nus.edu>
2020-12-26 00:31:43 +0800

    Initial Commit

---
+++ MemorizeTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizeTutorial
+//
+//  Created by lum jian yang sean on 26/12/20.
+//  Copyright © 2020 sean lum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
